b438f8095b3728c3af7919b4856f60eca4980ea9
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-07-29 15:33:36 -0300

    Rest Config

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSrializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Danilo'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+        """Exibindo todos os cursos"""
+        queryset = Curso.objects.all()
+        serializer_class = CursoSrializer
 
 # Create your views here.
--- setup/urls.py
+++ setup/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
-from escola.views import alunos
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos)
+    path('', include(router.urls))
 ]
