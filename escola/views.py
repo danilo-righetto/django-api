68dd3431428d0f738c569d50e75639ecf8630070
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-08-01 20:46:53 -0300

    Updating resources

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from serializer import AlunoSerializer, CursoSrializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSrializer, MatriculaSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -11,5 +11,11 @@ class CursosViewSet(viewsets.ModelViewSet):
         """Exibindo todos os cursos"""
         queryset = Curso.objects.all()
         serializer_class = CursoSrializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Listando todas as matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
 
 # Create your views here.
--- setup/urls.py
+++ setup/urls.py
@@ -13,17 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
-from escola.views import AlunosViewSet, CursosViewSet
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
 router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls) )
 ]
