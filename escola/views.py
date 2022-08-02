8075708bfc2256075af41241109d5978f8348f78
Danilo Righetto <danilo.righetto@laborit.com.br>
2022-08-01 21:34:11 -0300

    Course registrations

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSrializer, MatriculaSerializer, ListaMatriculasAlunosSerializer
+from escola.serializer import AlunoSerializer, CursoSrializer, MatriculaSerializer, ListaMatriculasAlunosSerializer, ListaAlunosMatriculadosSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -23,6 +23,13 @@ class ListaMatriculasAluno(generics.ListAPIView):
          queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
          return queryset
     serializer_class = ListaMatriculasAlunosSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos e alunas matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
 
 
 # Create your views here.
